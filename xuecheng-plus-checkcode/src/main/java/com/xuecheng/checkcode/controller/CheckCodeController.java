13f53f487b4a3dcade85e5f79d88c3570e9a8026
differencer <1559114644@qq.com>
2023-06-26 10:08:20 +0800

    主体工程完成认证登录功能完成

---
+++ xuecheng-plus-checkcode/src/main/java/com/xuecheng/checkcode/controller/CheckCodeController.java
@@ -0,0 +1,81 @@
+package com.xuecheng.checkcode.controller;
+
+import com.xuecheng.base.model.RestResponse;
+import com.xuecheng.checkcode.model.CheckCodeParamsDto;
+import com.xuecheng.checkcode.model.CheckCodeResultDto;
+import com.xuecheng.checkcode.model.FindPswDto;
+import com.xuecheng.checkcode.model.RegisterDto;
+import com.xuecheng.checkcode.service.CheckCodeService;
+import com.xuecheng.checkcode.service.RegisterService;
+import com.xuecheng.checkcode.service.SendCodeService;
+import com.xuecheng.checkcode.service.VerifyService;
+import com.xuecheng.checkcode.utils.MailUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 验证码服务接口
+ * @date 2022/9/29 18:39
+ */
+@Api(value = "验证码服务接口")
+@RestController
+public class CheckCodeController {
+
+    @Resource(name = "PicCheckCodeService")
+    private CheckCodeService picCheckCodeService;
+
+
+    @ApiOperation(value="生成验证信息", notes="生成验证信息")
+    @PostMapping(value = "/pic")
+    public CheckCodeResultDto generatePicCheckCode(CheckCodeParamsDto checkCodeParamsDto){
+        return picCheckCodeService.generate(checkCodeParamsDto);
+    }
+
+    @ApiOperation(value="校验", notes="校验")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "业务名称", required = true, dataType = "String", paramType="query"),
+            @ApiImplicitParam(name = "key", value = "验证key", required = true, dataType = "String", paramType="query"),
+            @ApiImplicitParam(name = "code", value = "验证码", required = true, dataType = "String", paramType="query")
+    })
+    @PostMapping(value = "/verify")
+    public Boolean verify(String key, String code){
+        Boolean isSuccess = picCheckCodeService.verify(key,code);
+        return isSuccess;
+    }
+
+    @Autowired
+    VerifyService verifyService;
+
+    @Autowired
+    SendCodeService sendCodeService;
+
+    @ApiOperation(value = "发送邮箱验证码", tags = "发送邮箱验证码")
+    @PostMapping("/phone")
+    public void sendEMail(@RequestParam("param1") String email) {
+        String code = MailUtil.achieveCode();
+        sendCodeService.sendEMail(email, code);
+    }
+
+    @ApiOperation(value = "找回密码", tags = "找回密码")
+    @PostMapping("/findpassword")
+    public void findPassword(@RequestBody FindPswDto findPswDto) {
+        verifyService.findPassword(findPswDto);//
+        //
+    }
+    @Autowired
+    RegisterService registerService;
+    @ApiOperation(value = "注册", tags = "注册")
+    @PostMapping("/register")
+    public void register(@RequestBody RegisterDto registerDto) {
+        registerService.register(registerDto);
+    }
+}
